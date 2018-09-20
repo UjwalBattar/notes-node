401574410e81b6eccf2abbb2ed2f2f8edaae0fd9
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-20 13:30:54 -0700

    add functionality to notes add, remove, list, read

--- notes.js
+++ notes.js
@@ -1,23 +1,52 @@
-console.log('Starting notes.js');   
+/*jshint esversion: 6 */
+console.log('Starting notes.js');
+
+const fs = require('fs');
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (err) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
 
 var addNote = (title, body) => {
-    console.log("Adding note:", title, body);
-    
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (!duplicateNotes.length) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 
 var getAll = () => {
-    console.log('Getting all notes...');
-    
+    return fetchNotes();
 };
 
 var getNote = (title) => {
-    console.log('Here is the note you requested: ', title);
-    
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
-    console.log('Removing the note with the title: ', title);
-    
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+
+    return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
